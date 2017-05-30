eea798d982d9d217193384522d4d2e692736562a
abdemuqsit <abdemuqsit@hotmail.com>
2017-05-30 21:22:20 +0500

    Add tags enum

---
+++ AbdulMuqsit.Fa11BCS008.Compiler.Lexer/Tag.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbdulMuqsit.Fa11BCS008.Compiler.Lexer
+{
+    public enum Tag 
+    {
+
+        AND,
+        BASIC,
+        BREAK,
+        DO,
+        ELSE,
+        Equal,
+        FALSE,
+        GreaterThanOrEqual,
+        ID,
+        IF,
+        LessThanOrEqual,
+        NotEqual,
+        NUM,
+        OR,
+        REAL,
+        TRUE,
+        WHILE,
+        UnaryAnd,
+        UnaryOr,
+        UnaryNot,
+        Assignment,
+        GreaterThan,
+        LessThan
+
+    }
+}
