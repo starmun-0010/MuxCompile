0f7cde8270c598554530728a53a51aca60ebfd11
abdemuqsit <abdemuqsit@hotmail.com>
2017-05-30 21:24:00 +0500

    Add words list

---
+++ AbdulMuqsit.Fa11BCS008.Compiler.Lexer/Words.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbdulMuqsit.Fa11BCS008.Compiler.Lexer
+{
+    public static class Words
+    {
+        public static Word And { get; }
+        public static Word Or { get; }
+        public static Word Equal { get; }
+        public static Word NotEqual { get; }
+        public static Word LessThanOrEqual { get; }
+        public static Word GreaterThanOrEqual { get; }
+        public static Word True { get; }
+        public static Word False { get; }
+        public static Word IF { get; }
+        public static Word Else { get; }
+        public static Word While { get; }
+        public static Word Do { get; }
+        public static Word Break { get; }
+
+    }
+}
