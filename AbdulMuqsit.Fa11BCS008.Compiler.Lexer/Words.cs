66ab51d71d05783c5f5b1e3efb860b236540e0c8
abdemuqsit <abdemuqsit@hotmail.com>
2017-05-31 01:36:51 +0500

    Initialize word class

--- AbdulMuqsit.Fa11BCS008.Compiler.Lexer/Words.cs
+++ AbdulMuqsit.Fa11BCS008.Compiler.Lexer/Words.cs
@@ -7,19 +7,19 @@ namespace AbdulMuqsit.Fa11BCS008.Compiler.Lexer
 {
     public static class Words
     {
-        public static Word And { get; }
-        public static Word Or { get; }
-        public static Word Equal { get; }
-        public static Word NotEqual { get; }
-        public static Word LessThanOrEqual { get; }
-        public static Word GreaterThanOrEqual { get; }
-        public static Word True { get; }
-        public static Word False { get; }
-        public static Word IF { get; }
-        public static Word Else { get; }
-        public static Word While { get; }
-        public static Word Do { get; }
-        public static Word Break { get; }
+        public static Word And { get; } = new Word("$$", Tag.AND);
+        public static Word Or { get; } = new Word("||", Tag.OR);
+        public static Word Equal { get; } = new Word("==", Tag.Equal);
+        public static Word NotEqual { get; } = new Word("!=", Tag.NotEqual);
+        public static Word LessThanOrEqual { get; } = new Word("<=", Tag.LessThanOrEqual);
+        public static Word GreaterThanOrEqual { get; } = new Word(">=", Tag.GreaterThanOrEqual);
+        public static Word True { get; } = new Word("true", Tag.TRUE);
+        public static Word False { get; } = new Word("false", Tag.FALSE);
+        public static Word IF { get; } = new Word("if", Tag.IF);
+        public static Word Else { get; } = new Word("else", Tag.ELSE);
+        public static Word While { get; } = new Word("while", Tag.WHILE);
+        public static Word Do { get; } = new Word("do", Tag.DO);
+        public static Word Break { get; } = new Word("break", Tag.BREAK);
 
     }
 }
