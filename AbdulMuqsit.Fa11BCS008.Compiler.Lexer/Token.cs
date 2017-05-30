2fc6631b0938a5d4d065de153a477369179172f6
abdemuqsit <abdemuqsit@hotmail.com>
2017-05-30 21:22:45 +0500

    Add I token and token definition

---
+++ AbdulMuqsit.Fa11BCS008.Compiler.Lexer/Token.cs
@@ -0,0 +1,28 @@
+﻿using AbdulMuqsit.Fa11BCS008.Compiler.Lexer.Contracts;
+
+namespace AbdulMuqsit.Fa11BCS008.Compiler.Lexer
+{
+    public class Token : IToken
+    {
+
+        /* one for each keyword
+*
+* can be set can be one for each operator
+* 
+* one for all identifiers
+* 
+* one or more for constants, (numbers, literal strings)
+* 
+* one for each punctuation symbol ({}(),;)
+*
+* */
+        public int Tag { get; set; }
+
+        public Token(int t) => Tag = t;
+
+        public Token(Tag tag)
+        {
+            Tag = (int)tag;
+        }
+    }
+}
