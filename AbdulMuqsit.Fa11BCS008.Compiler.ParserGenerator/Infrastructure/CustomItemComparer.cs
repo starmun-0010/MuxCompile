0ae730217179beb2461536e8f13aa4daf78997de
abdemuqsit <abdemuqsit@hotmail.com>
2017-06-03 05:37:10 +0500

    add custom equality comparers for item and list of item

---
+++ AbdulMuqsit.Fa11BCS008.Compiler.ParserGenerator/Infrastructure/CustomItemComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbdulMuqsit.Fa11BCS008.Compiler.Common;
+
+namespace AbdulMuqsit.Fa11BCS008.Compiler.ParserGenerator.Infrastructure
+{
+    public class CustomItemComparer : IEqualityComparer<Item>
+    {
+        public bool Equals(Item x, Item y)
+        {
+            return x.NonTerminal == y.NonTerminal && x.Rule.SequenceEqual(y.Rule);
+        }
+
+        public int GetHashCode(Item obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ AbdulMuqsit.Fa11BCS008.Compiler.ParserGenerator/Infrastructure/CustomListOfItemComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbdulMuqsit.Fa11BCS008.Compiler.Common;
+
+namespace AbdulMuqsit.Fa11BCS008.Compiler.ParserGenerator.Infrastructure
+{
+    public class CustomListOfItemComparer : IEqualityComparer<List<Item>>
+    {
+        public bool Equals(List<Item> x, List<Item> y)
+        {
+            for (int i = 0; i < y.Count; i++)
+            {
+                if (x[i] != y[i])
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+        public int GetHashCode(List<Item> obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
