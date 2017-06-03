6869f09c07537a06763505f387d722ce79c896fe
abdemuqsit <abdemuqsit@hotmail.com>
2017-06-03 05:36:48 +0500

    Update item == operator overload

--- AbdulMuqsit.Fa11BCS008.Compiler.Common/Item.cs
+++ AbdulMuqsit.Fa11BCS008.Compiler.Common/Item.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AbdulMuqsit.Fa11BCS008.Compiler.Common
 {
@@ -6,6 +7,24 @@ namespace AbdulMuqsit.Fa11BCS008.Compiler.Common
     {
 
 
-        
+        public static bool operator ==(Item x, Item y)
+        {
+            return x.NonTerminal == y.NonTerminal && x.Rule.SequenceEqual(y.Rule);
+
+        }
+        public static bool operator !=(Item x, Item y)
+        {
+            return x.NonTerminal != y.NonTerminal || !x.Rule.SequenceEqual(y.Rule);
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
